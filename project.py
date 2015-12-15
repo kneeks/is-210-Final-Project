143234b2ab7e44705faa5144b8ae7a85dbb413a3
Niko Vaccaro <mr.nikovaccaro@gmail.com>
2015-12-15 09:24:05 +0000

    add prject file

--- project.py
+++ project.py
@@ -5,28 +5,40 @@
 ERROR = ('Sorry, you did not meet the criteria.'
         'Thank you for you time and good luck!')
 COUNT = 0
-Q1 = raw_input('What is your name? ').upper()
-Q2 = raw_input('What is your phone number? ')
-Q3 = raw_input('Do you like sports? ')[:1].lower()
-Q4 = raw_input('Do you like video games? ')[:1].lower()
+Q1 = 'What is your name? '
+Q2 = 'What is your phone number? '
+Q3 = 'Do you like sports? '
+Q4 = 'Do you like video games? '
+Q5 = 'Do you like meeting new people? '
+Q6 = 'Do you mind sharing a living space? '
+Q7 = 'Can you afford your share of rent? '
 
-if Q3 == 'y' or Q4 == 'y':
-    Q5 = raw_input('Do you like meeting new people? ')[:1].lower()
-    Q6 = raw_input('Do you mind sharing a living space? ')[:1].lower()
-    Q7 = raw_input('Can you afford your share of rent? ')[:1].lower()
-    ANSWERS = [Q3, Q4, Q5, Q6, Q7]
+A1 = raw_input(Q1).upper()
+A2 = raw_input(Q2)
+A3 = raw_input(Q3)[:1].lower()
+A4 = raw_input(Q4)[:1].lower()
+
+if A3 == 'y' or A4 == 'y':
+    A5 = raw_input(Q5)[:1].lower()
+    A6 = raw_input(Q6)[:1].lower()
+    A7 = raw_input(Q7)[:1].lower()
+    ANSWERS = [A3, A4, A5, A6, A7]
     for Y in ANSWERS:
         if Y == 'y':
             COUNT += 1
+
         else:
             pass
     if COUNT >= 3:
         f = open('Candidates.txt', 'a')
-        f.write(('Name: {0}\nPhone Number: {1}\n\n').format(Q1, Q2))
+        f.write(('Name: {0}\nPhone Number: {1}\n{2}{3}\n{4}{5}\n{6}{7}\n'
+                '{8}{9}\n{10}{11}\n\n').format(A1, A2, Q3, A3,
+                                               Q4, A4, Q5, A5,
+                                               Q6, A6, Q7, A7))
         f.close()
         print '\n\n\nCongrats. You have been selected as a candidate!'
     else:
-        pass
+        print ERROR
 else:
     print ERROR
 
